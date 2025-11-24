342a249067088517ace7f1f9cc02632923e4f1be
vitorfuziwara <vitor_fuziwara@outlook.com>
2025-11-24 15:50:27 -0300

    tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,14 +1,14 @@
 
-class Tabuleiro():
-    def __init__(self) -> None:
-        self.NUM_LINHAS = 6
-        self.NUM_COLUNAS = 7
-        self.PECA_VAZIA = '.'
-        self.tabuleiro = [
+class Tabuleiro(): #Declara a classe do tabuleiro
+    def __init__(self) -> None: 
+        self.NUM_LINHAS = 6 #Determina o número de linhas do tabuleiro
+        self.NUM_COLUNAS = 7 #Determina o número de colunas do tabuleiro
+        self.PECA_VAZIA = '.' #Determina o caractere que representa os espaços vazios do tabuleiro
+        self.tabuleiro = [ #Inicia a matriz do tabuleiro 
             [self.PECA_VAZIA for _ in range(self.NUM_COLUNAS)] 
             for _ in range(self.NUM_LINHAS)
         ]
-    def obter_linha_livre(self, coluna: int) -> int:
+    def obter_linha_livre(self, coluna: int) -> int: #Método que recebe o índice da coluna e retorna o índice da linha vazia mais baixa
         for linha in range(self.NUM_LINHAS - 1, -1, -1):
             
             if self.tabuleiro[linha][coluna] == self.PECA_VAZIA:
@@ -16,7 +16,7 @@ class Tabuleiro():
         
         return -1
     
-    def tabuleiro_cheio(self) -> bool:
+    def tabuleiro_cheio(self) -> bool: #Método que verifica se todas as colunas estão cheias
         linha_topo = self.tabuleiro[0]
         for i in linha_topo:
             if i == self.PECA_VAZIA:
@@ -24,7 +24,7 @@ class Tabuleiro():
         return True
 
     
-    def mostrar_tabuleiro(self):
+    def mostrar_tabuleiro(self): #Mostra o tabuleiro na tela e suas devidas coordenadas
         CANTO = "🪤"
         print("\n  0 1 2 3 4 5 6")
         print(f" {CANTO}-------------{CANTO}")
@@ -35,10 +35,10 @@ class Tabuleiro():
         print(f" {CANTO}-------------{CANTO}")
 
 
-    def fazer_jogada(self, linha:int, coluna:int, peca:str):
+    def fazer_jogada(self, linha:int, coluna:int, peca:str): #Coloca a peça na posição escolhida
         self.tabuleiro[linha][coluna] = peca
 
-    def verificar_vitoria(self, linha:int, coluna:int, peca:str) -> bool:
+    def verificar_vitoria(self, linha:int, coluna:int, peca:str) -> bool: #Verifica se a jogada realizada resultou na vitória do jogador que a realizou
         if (self._checar_horizontal(linha, peca) or 
             self._checar_vertical(coluna, peca) or
             self._checar_diagonal_principal(linha, coluna, peca) or
@@ -48,7 +48,7 @@ class Tabuleiro():
         
         return False
     
-    def _checar_horizontal(self,linha:int,peca:str) -> bool:
+    def _checar_horizontal(self,linha:int,peca:str) -> bool: #Checa se houve 4 peças na horizontal
         contador = 0
         for c in range(self.NUM_COLUNAS):
             if self.tabuleiro[linha][c] == peca:
@@ -59,7 +59,7 @@ class Tabuleiro():
                 contador = 0
         return False
     
-    def _checar_vertical(self,coluna:int,peca:str) -> bool:
+    def _checar_vertical(self,coluna:int,peca:str) -> bool: #Checa se houve 4 peças na vertical
         contador = 0
         for c in range(self.NUM_LINHAS):
             if self.tabuleiro[c][coluna] == peca:
@@ -70,7 +70,7 @@ class Tabuleiro():
                 contador = 0
         return False
     
-    def _checar_diagonal_principal(self,linha:int,coluna:int,peca:str) -> bool:
+    def _checar_diagonal_principal(self,linha:int,coluna:int,peca:str) -> bool: #Testa se houve 4 peças na diagonal principal
         temp_lin, temp_col = linha, coluna
         
         while temp_lin > 0 and temp_col > 0:
@@ -95,7 +95,7 @@ class Tabuleiro():
             
         return False
     
-    def _checar_diagonal_secundaria(self,linha:int,coluna:int,peca:str) -> bool:
+    def _checar_diagonal_secundaria(self,linha:int,coluna:int,peca:str) -> bool: #Testa se houve 4 peças na diagonal secundária
         temp_lin , temp_col = linha, coluna
         while temp_lin > 0 and temp_col < self.NUM_COLUNAS - 1:
             temp_lin -= 1
@@ -121,5 +121,6 @@ class Tabuleiro():
 
 
         
+
 
         
